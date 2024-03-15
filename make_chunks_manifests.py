258f654a389225b2d3ef75a8fb1bd795d57af87f
valcorrea <vbejar23@student.aau.dk>
2024-03-15 11:57:06 +0100

    added the dataset and changes in make_chunks

--- make_chunks_manifests.py
+++ make_chunks_manifests.py
@@ -18,7 +18,7 @@ import numpy as np
 import pandas as pd
 import tqdm
 
-# python make_chunks_manifests.py --dataset_path dataset/valid_dataset/ --train_csv metadata/train.csv --val_csv metadata/validation.csv --test_csv metadata/test.csv --train_chunks_dir train_24000_chunks --val_chunks_dir validation_24000_chunks --test_chunks_dir test_24000_chunks --output_dir chunks_meta_dir
+# python make_chunks_manifests.py --dataset_path dataset/valid_dataset/ --train_csv metadata/train.csv --val_csv metadata/validation.csv --test_csv metadata/test.csv --train_chunks_dir train_24000_chunks --val_chunks_dir validation_24000_chunks --test_chunks_dir test_24000_chunks --output_dir chunks_meta_dir --nl_csv  metadata/no_labeled.csv --nl_chunks_dir no_labeled_24000_chunks
 parser = argparse.ArgumentParser()
 parser.description = "Script to make experiment manifests"
 parser.add_argument(
@@ -47,6 +47,13 @@ parser.add_argument(
     help="path to test.csv file found in metadata",
 )
 parser.add_argument(
+    "--nl_csv",
+    type=str,
+    default=None,
+    help="path to no_labeled.csv file found in metadata",
+)
+
+parser.add_argument(
     "--train_chunks_dir", type=str, help="path to directory containing train chunks"
 )
 parser.add_argument(
@@ -60,6 +67,12 @@ parser.add_argument(
     type=str,
     default=None,
     help="path to directory containing test chunks",
+)
+parser.add_argument(
+    "--nl_chunks_dir",
+    type=str,
+    default=None,
+    help="path to directory containing no_labeled chunks",
 )
 parser.add_argument(
     "--output_dir",
@@ -114,6 +127,27 @@ def process_set(csv_path, chuck_path, meta_name):
         json.dump(lbl_map, fd)
 
 
+def process_set_nolabels(csv_path, chuck_path, meta_name):
+
+    # setdf = pd.read_csv(csv_path)
+    # set_files = setdf["uuid"].values
+    set_chunks_dir = chuck_path
+    chunk_files = []
+    set_chunk_files = glob.glob(os.path.join(set_chunks_dir, "*.wav"))
+
+    for f in tqdm.tqdm(set_chunk_files):
+        chunk_files.append(f)
+
+    chunk_files = np.asarray(chunk_files)
+
+    set_chunk = pd.DataFrame()
+    set_chunk["files"] = chunk_files
+
+    set_chunk = set_chunk.iloc[np.random.permutation(len(set_chunk))]
+
+    set_chunk.to_csv(os.path.join(args.output_dir, meta_name), index=False)
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
@@ -125,6 +159,8 @@ if __name__ == "__main__":
     args.val_chunks_dir = os.path.join(args.dataset_path, args.val_chunks_dir)
     args.test_chunks_dir = os.path.join(args.dataset_path, args.test_chunks_dir)
     args.output_dir = os.path.join(args.dataset_path, args.output_dir)
+    args.nl_csv = os.path.join(args.dataset_path, args.nl_csv)
+    args.nl_chunks_dir = os.path.join(args.dataset_path, args.nl_chunks_dir)
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
@@ -137,3 +173,4 @@ if __name__ == "__main__":
         process_set(args.train_csv, args.train_chunks_dir, "train_chunk.csv")
         process_set(args.val_csv, args.val_chunks_dir, "val_chunk.csv")
         process_set(args.test_csv, args.test_chunks_dir, "test_chunk.csv")
+        process_set_nolabels(args.nl_csv, args.nl_chunks_dir, "no_labeled_chunk.csv")
