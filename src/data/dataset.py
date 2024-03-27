8151283bd6bbec8f8b20a3505fed62b93815ec34
vismiroglou <vismiroglou@gmail.com>
2024-03-27 12:07:13 +0100

    Added SpecFeatDataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -212,7 +212,150 @@ class SpectrogramDataset(Dataset):
         sampler = WeightedRandomSampler(weights, len(weights))
         return sampler
             
+class SpecFeatDataset(Dataset):
+    def __init__(
+        self,
+        manifest_path: str,
+        metadata_path: str,
+        labels_map: str,
+        audio_config: dict,
+        augment: bool,
+        mask_param: int = 15, 
+
+        ) -> None:
+        super(SpecFeatDataset, self).__init__()
+
+        assert manifest_path is not None, 'Manifest path is required.'
+        assert metadata_path is not None, 'Metadata path is required'
+        assert labels_map is not None, 'Labels map is required'
+        assert audio_config is not None, 'Audio config is required.'
+
+        # Read manifest, metadata, labels map
+        self.get_encoded_metadata(metadata_path)
+        with open(labels_map) as f:
+            self.labels_map = json.load(f)
+        manifest = pd.read_csv(manifest_path)
+        self.files = manifest["files"].values
+        self.labels = manifest["labels"].values
+        assert len(self.files) == len(self.labels), 'File-label length mismatch.'
+
+        self.get_audio_config(audio_config)
+        self.get_spec_parser()
+        self.augment = augment
+        if self.augment:
+            self.mask_param = mask_param
+            self.get_augments()
+
+        self.get_weighted_sampler()
+
+    def get_encoded_metadata(self, metadata_path):
+        metadata = pd.read_csv(metadata_path)
+        for column in ['gender', 'respiratory_condition', 'fever_muscle_pain']:
+            metadata[column] = metadata[column].astype('category').cat.codes
+        self.metadata = metadata
+
+    def get_weighted_sampler(self):
+        from torch.utils.data import WeightedRandomSampler
+        # Initialize an empty array for weights
+        weights = []
+        
+        # Calculate label counts
+        label_counts = {}
+        for labels in self.labels:
+            for label in labels:
+                label_counts[label] = label_counts.get(label, 0) + 1
+                
+        for labels in self.labels:
+            weight = sum(1.0 / label_counts[label] for label in labels)
+            weights.append(weight)
+        
+        weights = torch.FloatTensor(weights)
+        sampler = WeightedRandomSampler(weights, len(weights))
+        self.weighted_sampler = sampler
+
+    def get_augments(self):
+        self.mask_param = self.mask_param
+        random.seed(42)
+        torch.manual_seed(42)
+        self.time_mask = TimeMasking(time_mask_param=self.mask_param, iid_masks=True)
+        self.freq_mask = FrequencyMasking(freq_mask_param=self.mask_param, iid_masks=True)
+
+    def get_spec_parser(self):
+        self.spec_parser = AudioParser(
+            n_fft=self.n_fft,
+            win_length=self.win_len,
+            hop_length=self.hop_len,
+            feature=self.feature,
+        )
     
+    def get_audio_config(self, audio_config):
+        self.sr = audio_config.get("sample_rate", "24000")
+        self.n_fft = audio_config.get("n_fft", 511)
+        self.win_len = audio_config.get("win_len", self.n_fft)
+        self.hop_len = audio_config.get("hop_len", self.n_fft//2)
+        self.normalize = audio_config.get("normalize", True)
+        self.min_duration = audio_config.get("min_duration", None)
+        self.feature = audio_config.get("feature", "spectrogram")
+
+    def transform(self, spec):
+        spec = torch.from_numpy(spec)
+        spec = spec.expand(1, -1, -1)
+        return spec
+    
+    def masking(self, spec):
+        for mask in range(2):
+            spec = self.time_mask(spec)
+            spec = self.freq_mask(spec)
+        return spec
+    
+    def __get_audio__(self, f):
+        audio = load_audio(f, self.sr, self.min_duration)
+        return audio
+
+    def __get_feature__(self, audio: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        real, comp = self.spec_parser(audio)
+        return real, comp
+    
+    def __parse_labels__(self, lbl: str) -> torch.Tensor:
+        label_tensor = torch.zeros(len(self.labels_map)).float()
+        label_tensor[self.labels_map[lbl]] = 1
+        return label_tensor
+
+    def __get_feats__(self, f):
+        chunk_id = os.path.basename(f).replace('wav', '')
+        mask = [uuid in chunk_id for uuid in self.metadata.uuid]
+        feats = self.metadata[mask][['SNR', 
+                                     'age', 
+                                     'gender', 
+                                     'respiratory_condition', 
+                                     'fever_muscle_pain']].values.flatten().tolist()
+        return torch.tensor(feats)
+
+    def __get_item_helper__(
+        self, index: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        
+        f = self.files[index]
+        preprocessed_audio = self.__get_audio__(f)
+        real, comp = self.__get_feature__(preprocessed_audio)
+    
+        lbl = self.labels[index]
+        label_tensor = self.__parse_labels__(lbl)
+
+        feats = self.__get_feats__(f)
+        return real, comp, feats, label_tensor
+        
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        real, comp, feats, targets = self.__get_item_helper__(index)
+        real = self.transform(real)
+        if self.augment:
+            real = self.masking(real)
+
+        return real, feats, targets
+    
+    def __len__(self):
+        return len(self.files)
+
 if __name__ == '__main__':
     from src.utils.config_parser import parse_config
     from torch.utils.data import DataLoader
