28d1ed2fb161013147c20791722976861ccc23ed
vikki <vismir23@student.aau.dk>
2024-04-01 15:40:02 +0200

    Implemeted extra_feats

--- light_train.py
+++ light_train.py
@@ -8,46 +8,8 @@ from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 
 from utils.config_parser import parse_config
-from utils.light_modules import LightningKWT
-from src.models.KWT import KWT
-from src.data.dataset import SpectrogramDataset
 
-
-def training_pipeline(config, logger, ckpt_path):
-
-    # Set device
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps" if torch.backends.mps.is_available() else "cpu"
-    )
-
-    # Initialize ViT
-    vit = KWT(**config['hparams']['KWT'])
-    vit.to(device);
-
-    # Initialize lightning module from checkpoint if provided
-    if ckpt_path:
-        model = LightningKWT.load_from_checkpoint(ckpt_path, model=vit, config=config)
-    else:
-        model = LightningKWT(model=vit, config=config)
-    model.to(device)
-
-    # Make datasets
-    train_set = SpectrogramDataset(manifest_path=config['tr_manifest_path'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=False)
-    val_set = SpectrogramDataset(manifest_path=config['val_manifest_path'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=False)
-
-    if config['dev_mode']:
-        train_set.files = train_set.files[:50]
-        train_set.len = len(train_set.files)
-        val_set.files = val_set.files[:50]
-        val_set.len = len(val_set.files)
-        config['hparams']['batch_size'] = 25
-
-    # Make dataloaders
-    train_loader = DataLoader(train_set, batch_size=config['hparams']['batch_size'], num_workers=5)
-    val_loader = DataLoader(val_set, batch_size=config['hparams']['batch_size'], num_workers=5)
-
+def training_pipeline(config, logger, model, train_loader, val_loader):
     # Create Callbacks
     model_checkpoint = ModelCheckpoint(monitor="val_loss", mode="min", verbose=True)
     early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=config['hparams']['early_stopping_patience'], verbose=True)
@@ -59,6 +21,51 @@ def training_pipeline(config, logger, ckpt_path):
                         log_every_n_steps=100,
                         strategy='ddp_find_unused_parameters_true')
     trainer.fit(model, train_loader, val_loader)
+
+def get_model(ckpt, extra_feats, config):
+    # Set device
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    if extra_feats:
+        from utils.light_modules import LightningKWT_extrafeats
+        if ckpt:
+            model = LightningKWT_extrafeats.load_from_checkpoint(ckpt, config)
+        else:
+            model = LightningKWT_extrafeats(config)
+    else:
+        from utils.light_modules import LightningKWT
+        if ckpt:
+            model = LightningKWT.load_from_checkpoint(ckpt, config)
+        else:
+            model = LightningKWT(config)
+    model.to(device)
+    return model
+
+def get_dataloaders(extra_feats, config):
+    # Make datasets
+    if extra_feats:
+        from src.data.dataset import SpecFeatDataset
+        train_set = SpecFeatDataset(manifest_path=config['tr_manifest_path'], metadata_path=config['metadata'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=True)
+        val_set = SpecFeatDataset(manifest_path=config['val_manifest_path'], metadata_path=config['metadata'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=False)
+    else:
+        from src.data.dataset import SpectrogramDataset
+        train_set = SpectrogramDataset(manifest_path=config['tr_manifest_path'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=True)
+        val_set = SpectrogramDataset(manifest_path=config['val_manifest_path'], labels_map=config['labels_map'], audio_config=config['audio_config'], augment=False)
+    
+    if config['dev_mode']:
+        train_set.files = train_set.files[:50]
+        train_set.len = len(train_set.files)
+        val_set.files = val_set.files[:50]
+        val_set.len = len(val_set.files)
+        config['hparams']['batch_size'] = 25
+
+    # Make dataloaders
+    train_loader = DataLoader(train_set, batch_size=config['hparams']['batch_size'], num_workers=5, sampler=train_set.weighted_sampler)
+    val_loader = DataLoader(val_set, batch_size=config['hparams']['batch_size'], num_workers=5, sampler=val_set.weighted_sampler)
+    return train_loader, val_loader
 
 def main(args):
 
@@ -86,11 +93,12 @@ def main(args):
                                 config=config["hparams"],
                                 log_model=True,
                                 save_dir='outputs/')
-        training_pipeline(config, logger, args.ckpt_path)
-
     else:
         logger = None
-        training_pipeline(config, logger, args.ckpt_path)
+    
+    model = get_model(args.ckpt, args.extra_feats, config)
+    train_loader, val_loader = get_dataloaders(args.extra_feats, config)
+    training_pipeline(config, logger, model, train_loader, val_loader)
 
 
 if __name__ == '__main__':
@@ -106,6 +114,7 @@ if __name__ == '__main__':
     ap.add_argument('--val_metadata', type=str, help='Path to metadata file')
     ap.add_argument('--ckpt_path', type=str, help='Path to model checkpoint.')
     ap.add_argument('--dev_mode', action='store_true', help='Flag to limit the dataset for testing purposes.')
+    ap.add_argument('--extra_feats', action='store_true', help='Whether to use the expanded version')
     args = ap.parse_args()
 
     main(args)
