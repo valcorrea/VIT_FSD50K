fc0e6b272bbdec67f3ffbb747327fa59e43a78d6
vismiroglou <vismiroglou@gmail.com>
2024-04-03 07:58:27 +0200

    Fixed an error when logging predictions with argmax dimension

--- utils/light_modules.py
+++ utils/light_modules.py
@@ -99,8 +99,7 @@ class LightningKWT(L.LightningModule):
         specs, targets = batch
         outputs = self(specs)
         loss = self.criterion(outputs, targets)
-        correct = outputs.argmax(1).eq(targets.argmax()).sum().float()
-
+        correct = outputs.argmax(1).eq(targets.argmax(1)).sum().float()
         self.log_dict({"train_loss": loss, "lr": self.optimizer.param_groups[0]["lr"],
                             "tr_correct_predictions": correct}, on_epoch=True, on_step=True, sync_dist=True)
         return loss
@@ -109,7 +108,7 @@ class LightningKWT(L.LightningModule):
         specs, targets = batch
         outputs = self(specs)
         val_loss = self.criterion(outputs, targets)
-        correct = outputs.argmax(1).eq(targets.argmax()).sum().float()
+        correct = outputs.argmax(1).eq(targets.argmax(1)).sum().float()
         accuracy = correct / len(batch)
         self.log_dict({"val_loss": val_loss, "val_correct_predictions": correct, "val_accuracy": accuracy}, on_epoch=True, on_step=True, sync_dist=True)
         return val_loss
