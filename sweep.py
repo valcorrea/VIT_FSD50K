5527022083615c1c9fbf1acbef974d008cca8735
vismiroglou <vismiroglou@gmail.com>
2024-04-25 09:57:56 +0200

    Added config merging functionality

--- sweep.py
+++ sweep.py
@@ -7,9 +7,25 @@ from utils.config_parser import parse_config
 
 
 def main(args):
-    model_config = parse_config(args.model_config)
-    sweep_config = parse_config(args.sweep_config)
-    pprint.pprint(sweep_config)
+    config = merge_config(args.model_config, args.sweep_config)
+    pprint.pprint(config)
+
+
+def merge_config(model_config:str, sweep_config:str) -> dict:
+    '''
+    Reads config files and merges the model and sweep configurations into one that contains the essentials.
+
+    Args:
+        model_config (str): Path to cfg. Contains the model architecture, audio config and experiment configs
+        sweep_config (str): Path to cfg. Contains the hyperparameters to sweep through and their possible values. 
+    
+    Returns:
+        model_config (dict): Contains the necessary combination of configs from model/sweep_config.
+    '''
+    model_config = parse_config(model_config)
+    sweep_config = parse_config(sweep_config)
+    model_config.update(sweep_config)
+    return model_config
 
 
 if __name__ == '__main__':
