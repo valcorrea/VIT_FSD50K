4b2827542793d0a01fe55369a6d5d12d77f27d7b
Sarthak Yadav <sarthak.yadav3@gmail.com>
2024-04-29 13:24:12 +0200

    fixed audio size issue

--- src/data/speechcommands_dataset.py
+++ src/data/speechcommands_dataset.py
@@ -14,15 +14,25 @@ class SpeechCommands(SPEECHCOMMANDS):
                  subset: str,
                  features: Callable = None,
                  download: bool = True,
+                 duration: float = 1.0
                  ) -> None:
         super().__init__(root, "speech_commands_v0.02", "SpeechCommands", download, subset)
         with open(labels_map, 'r') as fd:
                 self.labels_map = json.load(fd)
         self.features = features
+        self.duration = duration
 
     def __getitem__(self, n: int):
         items =  super().__getitem__(n)
         audio = items[0]
+        sr = items[1]
+        # SpeechCommands has a fixed duration of 1.
+        min_samples = int(sr * self.duration)
+        if audio.shape[1] < min_samples:
+            tile_size = (min_samples // audio.shape[1]) + 1
+            audio = audio.repeat(1, tile_size)
+            audio = audio[:, :min_samples]
+    
         label_str = items[2]
         label = self.labels_map[label_str]
         if self.features is not None:
