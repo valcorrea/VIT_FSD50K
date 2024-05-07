e3e1d6b0074b3185bcf4e9621c5931feb1b0b898
vismiroglou <vismiroglou@gmail.com>
2024-05-07 09:12:26 +0200

    Added augment flag

--- src/data/speechcommands_dataset.py
+++ src/data/speechcommands_dataset.py
@@ -6,7 +6,6 @@ import torchaudio
 import numpy as np
 import soundfile
 from glob import glob
-import random
 
 class SpeechCommands(SPEECHCOMMANDS):
     def __init__(self, 
@@ -27,7 +26,8 @@ class SpeechCommands(SPEECHCOMMANDS):
                  time_mask_width=25,
                  n_freq_masks=2,
                  freq_mask_width=7,
-                 seed=42
+                 seed=42,
+                 augment=False
                  ) -> None:
         super().__init__(root, "speech_commands_v0.02", "SpeechCommands", download, subset)
         '''
@@ -50,26 +50,9 @@ class SpeechCommands(SPEECHCOMMANDS):
         with open(labels_map, 'r') as fd:
                 self.labels_map = json.load(fd)
         
-        random.seed(seed)
         np.random.seed(seed)
 
         self.duration = duration
-        self.r_max = r_max
-        self.r_min = r_min
-
-        # Background
-        self.bg_files = glob(os.path.join(root, "SpeechCommands", "speech_commands_v0.02", "_background_noise_", '*.wav'))
-        self.bg_prob = bg_prob
-
-        # Time-shift Config
-        self.s_min = s_min
-        self.s_max = s_max
-
-        # Masking Config
-        self.n_time_masks = n_time_masks
-        self.time_mask_width = time_mask_width
-        self.n_freq_masks = n_freq_masks
-        self.freq_mask_width = freq_mask_width
 
         # Audio Config
         self.sr = audio_config.get('sr', 16000)
@@ -93,40 +76,58 @@ class SpeechCommands(SPEECHCOMMANDS):
             n_mels=self.n_mels, power=2.
         )
 
-    # def resample(self, x):
+        # Augmentation arguments
+        self.augment=augment
+        if self.augment:
+            # Resample
+            self.r_max = r_max
+            self.r_min = r_min
 
-    #     r_min = self.r_min
-    #     r_max = self.r_max
-    #     sr_new = int(self.sr * np.random.uniform(r_min, r_max))
-    #     x_resampled = torchaudio.transforms.Resample(self.sr, sr_new)(x)
-    #     return x_resampled
+            # Background
+            self.bg_files = glob(os.path.join(root, "SpeechCommands", "speech_commands_v0.02", "_background_noise_", '*.wav'))
+            self.bg_prob = bg_prob
+
+            # Time-shift
+            self.s_min = s_min
+            self.s_max = s_max
+
+            # Masking
+            self.n_time_masks = n_time_masks
+            self.time_mask_width = time_mask_width
+            self.n_freq_masks = n_freq_masks
+            self.freq_mask_width = freq_mask_width
+
+
+    def resample(self, x):
+        r_min = self.r_min
+        r_max = self.r_max
+        sr_new = int(self.sr * np.random.uniform(r_min, r_max))
+        x_resampled = torchaudio.transforms.Resample(self.sr, sr_new)(x)
+        return x_resampled
     
-    # def time_shift(self, x):
-    #     sr = self.sr
-    #     s_min = self.s_min
-    #     s_max = self.s_max
-    #     start = int(np.random.uniform(sr * s_min, sr * s_max))
-    #     if start >= 0:
-    #         shifted_x = torch.cat((x[:, start:], torch.FloatTensor(np.random.uniform(-0.001, 0.001, start)).unsqueeze(0)), dim=1)
-    #     else:
-    #         shifted_x = torch.cat((torch.FloatTensor(np.random.uniform(-0.001, 0.001, -start)).unsqueeze(0), x[:, :start]), dim=1)
-    #     return shifted_x
+
+    def time_shift(self, x):
+        sr = self.sr
+        s_min = self.s_min
+        s_max = self.s_max
+        start = int(np.random.uniform(sr * s_min, sr * s_max))
+        if start >= 0:
+            shifted_x = torch.cat((x[:, start:], torch.FloatTensor(np.random.uniform(-0.001, 0.001, start)).unsqueeze(0)), dim=1)
+        else:
+            shifted_x = torch.cat((torch.FloatTensor(np.random.uniform(-0.001, 0.001, -start)).unsqueeze(0), x[:, :start]), dim=1)
+        return shifted_x
+   
     
     def spec_augment(self, mel_spec):
-        n_time_masks = self.n_time_masks
-        time_mask_width = self.time_mask_width
-        n_freq_masks = self.n_freq_masks
-        freq_mask_width = self.freq_mask_width
-
         mel_spec_copy = mel_spec.clone()  # Make a copy of the input mel_spec
 
-        for _ in range(n_time_masks):
-            offset = np.random.randint(0, time_mask_width)
+        for _ in range(self.n_time_masks):
+            offset = np.random.randint(0, self.time_mask_width)
             begin = np.random.randint(0, mel_spec_copy.shape[2] - offset)
             mel_spec_copy[:, :, begin: begin + offset] = 0.0
         
-        for _ in range(n_freq_masks):
-            offset = np.random.randint(0, freq_mask_width)
+        for _ in range(self.n_freq_masks):
+            offset = np.random.randint(0, self.freq_mask_width)
             begin = np.random.randint(0, mel_spec_copy.shape[1] - offset)
             mel_spec_copy[:, begin: begin + offset, :] = 0.0
 
@@ -134,9 +135,9 @@ class SpeechCommands(SPEECHCOMMANDS):
 
 
     def add_background(self, x):
-        bg_path = random.choice(self.bg_files)
+        bg_path = np.random.choice(self.bg_files)
         bg_wav = soundfile.read(bg_path)[0]
-        index = random.randint(0, len(bg_wav-len(x)))
+        index = np.random.randint(0, len(bg_wav-len(x)))
         background_cropped = torch.tensor(0.1*bg_wav[index:index+len(x)])
         wav_with_bg = x.add(background_cropped).float()
         return wav_with_bg
@@ -149,11 +150,13 @@ class SpeechCommands(SPEECHCOMMANDS):
         sr = items[1]
         
         # Add background
-        if random.random() < self.bg_prob:
-            audio = self.add_background(audio)
+        if self.augment:
+            if np.random.random() < self.bg_prob:
+                audio = self.add_background(audio)
 
-        # #resampling audio sample
-        # audio = self.resample(audio)
+        # Resample
+        # if self.augment:
+        #     audio = self.resample(audio)
 
         # SpeechCommands has a fixed duration of 1.
         min_samples = int(sr * self.duration)
@@ -165,8 +168,9 @@ class SpeechCommands(SPEECHCOMMANDS):
         label_str = items[2]
         label = self.labels_map[label_str]
 
-        # # Applying time shifting
-        # audio = self.time_shift(audio)
+        # # Time-shift
+        # if self.augment:
+        #     audio = self.time_shift(audio)
 
         # Computing log mel spectrogram
         audio = self.melspec(audio)
@@ -181,7 +185,8 @@ class SpeechCommands(SPEECHCOMMANDS):
             audio = (audio - mean) / (std + 1e-8)
 
         # Applying SpecAug
-        audio = self.spec_augment(audio)
+        if self.augment:
+            audio = self.spec_augment(audio)
         return audio, label
     
     
